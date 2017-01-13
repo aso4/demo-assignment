f7d64ff1cdab099f9df99c302f74211f54126fae
aso4 <albertyso@gmail.com>
2017-01-13 16:48:12 -0500

    move modals and messages to new js files, add scrollable area to message box and apply necessary css

---
+++ app/js/messages.js
@@ -0,0 +1,30 @@
+// Get a reference to the root of the chat data.
+var messagesRef = new Firebase('https://fir-assignment-420f1.firebaseio.com/');
+
+// When the user presses enter on the message input, write the message to firebase.
+// $('#messageButton').onclick(function(e) {
+//     if ( !$(this).val() ) {
+//         var text = $('#messageInput').val();
+//         messagesRef.push({
+//             text: text
+//         });
+//         $('#messageInput').val('');
+//     }
+// });
+
+$('#messageInput').keypress(function(e) {
+    if (e.keyCode == 13) {
+        var text = $('#messageInput').val();
+        messagesRef.push({
+            text: text
+        });
+        $('#messageInput').val('');
+    }
+});
+
+// Add a callback that is triggered for each chat message.
+messagesRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    $('<div/>').text(message.text).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+});
---
+++ app/js/modals.js
@@ -0,0 +1,32 @@
+ // Get the modal
+ var modal = document.getElementById('myModal');
+ var profileModal = document.getElementById('myProfileModal');
+ // Get the button that opens the modal
+ var btn = document.getElementById("expand-menu");
+ var btn2 = document.getElementById("expand-profile-menu");
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+ // When the user clicks on the button, open the modal
+ btn.onclick = function() {
+     modal.style.display = "block";
+ }
+ btn2.onclick = function() {
+     profileModal.style.display = "block";
+     }
+     // When the user clicks on <span> (x), close the modal
+ span.onclick = function() {
+     modal.style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
+
+ window.onclick = function(event) {
+     if (event.target == profileModal) {
+         profileModal.style.display = "none";
+     }
+ }
